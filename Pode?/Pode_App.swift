714764e678cf9487ac91dbbb2d1c0078163d3f16
Gabriel Gardini <gabrielgardini1@gmail.com>
2026-04-13 10:46:52 -0300

    Initial Commit

---
+++ Pode?/Pode_App.swift
@@ -0,0 +1,32 @@
+//
+//  Pode_App.swift
+//  Pode?
+//
+//  Created by Gabriel Gardini on 13/04/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Pode_App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Pode?Tests/Pode_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Pode_Tests.swift
+//  Pode?Tests
+//
+//  Created by Gabriel Gardini on 13/04/26.
+//
+
+import Testing
+@testable import Pode_
+
+struct Pode_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
