d4b3a99cea0faed03f1160a9f556a7a84600b57d
Gabriel Gardini <gabrielgardini1@gmail.com>
2026-04-13 12:05:22 -0300

    consegue ler textos de forma nao estruturada

--- Pode?/ContentView.swift
+++ Pode?/ContentView.swift
@@ -6,45 +6,13 @@
 //
 
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-    
     @State private var scannedText: String = ""
     @State private var presentScanner: Bool = false
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        presentScanner = true
-                    } label: {
-                        Label("Ler Tabela", systemImage: "camera.viewfinder")
-                    }
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
+        NavigationStack {
             VStack(alignment: .leading, spacing: 12) {
                 Text("Texto da Tabela Nutricional")
                     .font(.title3).bold()
@@ -55,29 +23,23 @@ struct ContentView: View {
                 }
             }
             .padding()
+            .navigationTitle("Pode?")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        presentScanner = true
+                    } label: {
+                        Label("Ler Tabela", systemImage: "camera.viewfinder")
+                    }
+                }
+            }
         }
         .sheet(isPresented: $presentScanner) {
             CameraTextScannerView(text: $scannedText)
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
         }
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
